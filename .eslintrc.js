bb339ae348004c6616af0075d7f6b4ed252816c2
coby walsh <cobywalsh7@yahoo.com>
2023-07-30 13:01:19 -0600

    added .eslintrc.js file to fix error in login.js file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     parser: 'babel-eslint',
     parserOptions: {
-      ecmaVersion: 2021, // Change this to the ECMAScript version you want to support (e.g., 2018, 2019, 2020, 2021, etc.)
+      ecmaVersion: 2021, // Change this to the ECMAScript version I want to support (e.g., 2018, 2019, 2020, 2021, etc.)
       sourceType: 'module',
     },
   };
