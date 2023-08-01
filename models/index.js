90906784fc0dcf170a9772359a63ab369237f11e
Adam Hansen <adamhansen@me.com>
2023-07-31 18:30:29 -0600

    Add additional relationships in ./modules/index.js

--- models/index.js
+++ models/index.js
@@ -13,12 +13,40 @@ Project.belongsTo(User, {
   foreignKey: 'fk_user',
 });
 
+Project.hasMany(Note, {
+  foreignKey: 'fk_project',
+  onDelete: 'CASCADE',
+});
+
+Note.belongsTo(Project, {
+  foreignKey: 'fk_project',
+});
+
+Project.hasMany(Image, {
+  foreignKey: 'fk_project',
+  onDelete: 'CASCADE',
+});
+
 Image.belongsTo(Project, {
   foreignKey: 'fk_project',
+});
+
+Material.hasMany(Image, {
+  foreignKey: 'fk_material',
 });
 
 Image.belongsTo(Material, {
   foreignKey: 'fk_material',
 });
+
+Material.hasMany(Note, {
+  foreignKey: 'fk_material',
+  onDelete: 'CASCADE',
+});
+
+Note.belongsTo(Material, {
+  foreignKey: 'fk_material',
+  onDelete: 'CASCADE',
+})
 
 module.exports = { Image, Material, Note, Project, User };
