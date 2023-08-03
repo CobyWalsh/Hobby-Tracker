38b8bb1ebd670fef50bce25cd13968c0a51445a9
Adam Hansen <adamhansen@me.com>
2023-08-02 19:35:13 -0600

    Install and require multer

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const multer = require('multer');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
