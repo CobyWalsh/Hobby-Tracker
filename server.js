d4e6a0cfb29ee89b6b26aab3bccb1f169e3135ea
Adam Hansen <adamhansen@me.com>
2023-08-02 22:40:37 -0600

    Update ./server.js

--- server.js
+++ server.js
@@ -9,16 +9,17 @@ const expressHandlebars = require('express-handlebars');
 const expressSession = require('express-session');
 const gm = require('gm');
 const multer = require('multer');
+const SequelizeStore = require('connect-session-sequelize')(
+  expressSession.Store,
+);
 const uuid = require('uuid');
 
 const routes = require('./controllers');
+const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const app = express();
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 
 const handlebars = expressHandlebars.create({ helpers });
 
@@ -26,7 +27,8 @@ const session = {
   // Load the session secret from the SESSION_SECRET variable in the .env file
   secret: process.env.SESSION_SECRET,
   cookie: {
-    maxAge: 300000,
+    // Set the maximum age for the cookie/session to be valid at 24 hours
+    maxAge: 1000 * 60 * 60 * 24,
     httpOnly: true,
     secure: false,
     sameSite: 'strict',
@@ -34,8 +36,8 @@ const session = {
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(expressSession(session));
@@ -50,5 +52,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(port, () => console.log('Now listening'));
 });
