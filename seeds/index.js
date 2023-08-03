738639f643de83cc0da61b0da784d71c3438462c
Spyrodragonman Mannyman <nate.is.cool.yeah@gmail.com>
2023-08-02 18:37:07 -0600

    finnished adding notes, materials, and image iteration generation in seeds/index.js file

--- seeds/index.js
+++ seeds/index.js
@@ -13,18 +13,39 @@ const imageData = require('./imageData.json');
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
+
+ //creating user records in database
   const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
 
+ //iterating over projects and creating records in database
   for (const project of projectData) {
     await Project.create({
       ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
 
+//iterating over notes and creating records in database
+
+  for (const note of noteData) {
+    await Note.create ({
+        ...note,
+    });
+  }
+//iterating over materials and creating records in database
+  for (const material of materialData) {
+    await Material.create ({
+        ...material,
+    });
+  }
+//iterating over images and creating records in database
+  for (const image of imageData) {
+    await Image.create ({
+        ...image,
+    });
+  }
   process.exit(0);
 };
 
