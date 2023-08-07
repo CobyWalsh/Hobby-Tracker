a83449fff23f48a6563c77daf4f48a395bc6c4ba
Adam Hansen <adamhansen@me.com>
2023-08-07 08:05:13 -0600

    Create and update import and use of /api/* routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,17 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
-const dashboardRoutes = require('./dashboardRoutes');
 
-router.use('/users', userRoutes);
+const dashboardRoutes = require('./dashboardRoutes.js');
+const imageRoutes = require('./imageRoutes.js');
+const materialRoutes = require('./materialRoutes.js');
+const noteRoutes = require('./noteRoutes.js');
+const projectRoutes = require('./projectRoutes.js');
+const userRoutes = require('./userRoutes.js');
+
+router.use('/dashboard', dashboardRoutes);
+router.use('/images', imageRoutes);
+router.use('/materials', materialRoutes);
+router.use('/notes', noteRoutes);
 router.use('/projects', projectRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
