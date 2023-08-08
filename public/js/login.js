79053be610a944f902b803fa1300c5e371e83927
02madmax <maxlamthach10@gmail.com>
2023-08-07 18:43:27 -0600

    added logout button

--- public/js/login.js
+++ public/js/login.js
@@ -41,18 +41,18 @@ const signupFormHandler = async (event) => {
   }
 };
 
-const logout = async () => {
-  const response = await fetch('/api/users/logout', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-  });
+// const logout = async () => {
+//   const response = await fetch('/api/users/logout', {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//   });
 
-  if (response.ok) {
-    window.location.replace('/');
-  } else {
-    alert(response.statusText);
-  }
-};
+//   if (response.ok) {
+//     window.location.replace('/');
+//   } else {
+//     alert(response.statusText);
+//   }
+// };
 
 document
   .querySelector('.login-form')
@@ -62,5 +62,5 @@ document
   .querySelector('.signup-form')
   .addEventListener('submit', signupFormHandler);
 
-  document
-  .querySelector('#logout').addEventListener('click', logout);
+  // document
+  // .querySelector('#logout').addEventListener('click', logout);
